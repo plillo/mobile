c210aeb013137481698eca49b179c388c45c23fa
plillo <paolo.lillo@hash.it>
2016-05-07 12:01:09 +0200

    Aggiornato controller news

--- js/news/controllers.js
+++ js/news/controllers.js
@@ -3,19 +3,16 @@
 
 angular.module('starter.news.controllers', ['ionic'])
 
-.controller('NewsPageCtrl', function($scope, $timeout, $stateParams, $ionicSideMenuDelegate, broker, ionicMaterialInk, ionicMaterialMotion) {
-	$ionicSideMenuDelegate.canDragContent(true);
+.controller('NewsPageCtrl', 
+        function($scope, $timeout, $stateParams, $ionicSideMenuDelegate, broker, ionicMaterialInk, ionicMaterialMotion) {
+    $ionicSideMenuDelegate.canDragContent(true);
 	
-	$scope.$parent.clearFabs();
+    $scope.$parent.clearFabs();
     $timeout(function() {
         $scope.$parent.hideHeader();
     }, 0);
     ionicMaterialMotion.fadeSlideInRight();
     ionicMaterialInk.displayEffect();
-   
-	$scope.getUrlBroker = function() {
-		alert(broker.getUrl()+':'+broker.getPort()+' ['+(broker.isConnected()?'':'NOT ')+'CONNECTED]');
-	};
 })
 
 ;
