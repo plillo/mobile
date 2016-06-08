8bc29a3ec4b7223c6195fe8ead09133da447dacf
Fabpk90 <fabrizio1996@live.fr>
2016-06-08 17:57:39 +0200

    Test

--- js/games/develop/tris/directives.js
+++ js/games/develop/tris/directives.js
@@ -3,15 +3,44 @@ angular.module('games.develop.directives', ['ngFileUpload']);
 
 //ADD 'gameTris' directive
 //........................
-angular.module('games.develop.directives').directive('gameTris', function(business, product) {
+angular.module('games.develop.directives').directive('gameTris', function() {
     return {
-        replace: false,
+        replace: true,
         scope: {
+
         },
-        template : '>>gameTris<<',
+        templateUrl : 'js/games/develop/tris/templates/gametris.html',
         controller: function($scope, $window, $element){
+          $scope.matTris = [[0,0,0],
+                            [0,0,0],
+                            [0,0,0]];
+
+          $scope.player = 1;
+
+          $scope.timer1 = 0.0;
+          $scope.timer2 = 0.0;
+
+          $scope.getNumber = function(num){
+            return new Array(num);
+          }
+
+          $scope.Play = function(){
+
+          };
+
+          $scope.setPlay = function(x, y){
+            //if the the matrix is free on the point
+            if(matTris[x][y] == 0){
+              matTris[x][y] = player;
+            }
+            else //the player can't play here
+             alert('you cannnot play this move!');
+          };
+
+
         },
         link: function(scope, element, attributes){
+
         }
     };
 });
